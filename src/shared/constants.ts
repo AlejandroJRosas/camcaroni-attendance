18ca0a23477dc22807674f5830abe77750322f3d
Alejandro Rosas <alejandrojrosas2001@gmail.com>
2024-11-01 17:04:32 -0400

    chore: change participation days for testing purposes

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -7,16 +7,16 @@ export const { PORT, SPREADSHEET_ID, GOOGLE_API_KEY, SHEET_NAME } =
 	validateEnv();
 
 export const FIRST_DAY_EVENT = {
-	date: 14,
+	date: 1,
 	row: "J",
 };
 
 export const SECOND_DAY_EVENT = {
-	date: 15,
+	date: 2,
 	row: "K",
 };
 
 export const THIRD_DAY_EVENT = {
-	date: 16,
+	date: 3,
 	row: "L",
 };
